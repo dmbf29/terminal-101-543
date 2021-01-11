a486dadcdd20301a0c5250c3fb1401399279e89a
Douglas Berkley <douglasmberkley@gmail.com>
2021-01-11 15:11:01 +0900

    initial commit. created cheatsheet and index

---
+++ cheatsheet.rb
@@ -0,0 +1,42 @@
+# pwd
+# => present working directory (where am I?)
+
+# ls / ll
+# => short list / long list
+
+# cd folder_name
+# => changes directory
+
+# cd .. / ..
+# => moves back out of folder
+
+# mkdir new_folder_name
+# => creates a new directory
+
+# touch filename
+# => creates a new file
+
+# rm filename
+# => removes file
+
+# rm -r folder_name
+# => removes folder
+# -f (to force remove)
+
+# mv folder/file folder_name
+# => move the folder or file
+
+# mv older/file new_name
+# => rename the file or folder
+
+# stt / open .
+# => opens our text editor
+
+# open .
+# => open with default program
+
+# cat filename
+# => prints file into terminal
+
+# curl url
+# => http request
